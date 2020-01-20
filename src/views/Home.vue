c7ce6afb0d1e7e11dbbbda86c0941e77e0779145
Arya Wiranata <Arya.wiranata@btpn.com>
2020-01-20 15:03:57 +0700

    Add catch error for getData methods

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -34,7 +34,7 @@ export default {
 
         return response.data.schema;
       } catch (error) {
-        console.error(error);
+        console.log(error);
         throw error;
       }
     },
--- tests/unit/components/Home.spec.js
+++ tests/unit/components/Home.spec.js
@@ -5,17 +5,14 @@ import DynamicForm from '@/components/DynamicForm.vue';
 jest.mock('axios');
 
 describe('Home', () => {
-  // Now mount the component and you have the wrapper
   let wrapper;
+  let data;
+  let mockGet;
+  let mockData;
+  let userData;
 
-  it('renders the DynamicForm component', () => {
-    wrapper = shallowMount(Home);
-    const actualResult = wrapper.contains(DynamicForm);
-    expect(actualResult).toBeTruthy();
-  });
-
-  it('should get schema by schema id from the server', async () => {
-    const data = {
+  beforeEach(() => {
+    data = {
       schema: {
         name: {
           type: 'text',
@@ -29,18 +26,65 @@ describe('Home', () => {
       }
     };
 
-    const mockGet = jest.fn();
+    userData = {
+      name: 'Izzy Engelbert Simanjuntak',
+      email: 'izzyengelbert@gmail.com',
+      phoneNumber: '0',
+      mailingList: 'Yes',
+      gender: 'Female',
+      dateOfBirth: '2020-01-01',
+      address: 'Jl. Rajawali 7 No. 8',
+      jobExperience: [
+        'Frontend Developer',
+        'UI/UX Engineer'
+      ]
+    };
+
+    mockGet = jest.fn();
     mockGet.mockResolvedValue({ data });
+
+    mockData = jest.fn();
+    mockData.mockRejectedValue({ userData });
 
     wrapper = shallowMount(Home, {
       mocks: {
         $http: {
-          get: mockGet
+          get: mockGet,
+          post: mockData
         }
       }
     });
+  });
 
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('renders the DynamicForm component', () => {
+    const actualResult = wrapper.contains(DynamicForm);
+    expect(actualResult).toBeTruthy();
+  });
+
+  it('should get schema by schema id from the server', async () => {
     await wrapper.vm.getFormData();
     expect(wrapper.vm.$http.get).toHaveBeenCalled();
+  });
+
+  it('should throw error if failed to fetch data', async () => {
+    const errorResponse = {
+      response: {
+        status: 400,
+        message: 'test'
+      }
+    };
+    mockGet.mockRejectedValueOnce(errorResponse);
+    // console.log(wrapper.vm.$http.get);
+
+    await expect(wrapper.vm.getFormData()).rejects.toEqual(errorResponse);
+  });
+
+  it('should post data to the server', async () => {
+    await wrapper.vm.saveData(mockData);
+    expect(wrapper.vm.$http.post).toHaveBeenCalled();
   });
 });
